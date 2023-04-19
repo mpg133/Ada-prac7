08d2504926786dba86c9e4c1545d22977440362b
proletario1312 <mpg133@alu.ua.es>
2023-04-19 18:45:08 +0200

    practica acabada

--- maze_greedy.cc
+++ maze_greedy.cc
@@ -10,14 +10,11 @@
 #include <queue>
 using namespace std ;
 
-const unsigned int SENTINEL = numeric_limits < unsigned int>:: max();
-bool NO_EXIT=true;
 
-struct position{
-    int x;
-    int y;
-};
+bool EXIT=true;
 
+
+//funció que comproba si la casella es valida
 bool isValid(unsigned int i, unsigned int j, unsigned int rows, unsigned int cols){
     if( (i>=0) && (j>=0) && (i<rows) && (j<cols)){
         return true;
@@ -26,9 +23,55 @@ bool isValid(unsigned int i, unsigned int j, unsigned int rows, unsigned int col
 }
 
 unsigned int maze_greedy(unsigned int i, unsigned int j, unsigned int rows, unsigned int cols,
-                        const vector<vector<int>> matriu ){
+                        const vector<vector<int>> matriu, vector<vector<int>> &aux_matriu ){
+
+    unsigned int valor=0;                       
+    //comprobar que pot començar
+    if(matriu[i][j]==0){
+        EXIT=false;
+        return 0; 
+    }else{
+            aux_matriu[i][j]=0;
+            valor++;
+        while(EXIT){
+
+            //cas base
+            if(i==rows-1 && j==cols-1){
+                
+                return valor;
+            }
+
+            
+            if(isValid(i+1,j+1,rows,cols) && matriu[i+1][j+1]==1 && aux_matriu[i+1][j+1]==-1){//mirem diagonal
+                
+                valor++;
+                i++;
+                j++;
+                aux_matriu[i][j]=0;
+            }else if(isValid(i+1,j,rows,cols) && matriu[i+1][j]==1 && aux_matriu[i+1][j]==-1){//miem baix
+                
+                valor++;
+                i++;
+                aux_matriu[i][j]=0;
 
 
+            }else if(isValid(i,j+1,rows,cols) && matriu[i][j+1]==1 && aux_matriu[i][j+1]==-1){//mirem dreta
+               
+                valor++;
+                j++;
+                aux_matriu[i][j]=0;
+
+            }else{//no te eixida
+                EXIT=false;
+                return 0;
+                
+            }
+
+
+        }
+
+        
+    }                     
 
 }
 
@@ -88,16 +131,27 @@ int main(int argc , char *argv[] ){
         //eixida de programa-----------------------------------------------------------
         //creem matriu auxiliar per al cami
         vector <vector<int>> aux_matriu (rows , vector<int>(cols) );
+         for(int i=0; i< rows;i++)
+            for(int j=0;j<cols;j++)
+                aux_matriu[i][j]=-1;
+
+        unsigned int resultat=maze_greedy(0,0,rows,cols,matriu,aux_matriu);
        
-        unsigned int resultat=maze_greedy(0,0,rows,cols,matriu);
-        if(resultat>0 && !NO_EXIT){
-            cout<<endl;
-        }else{
-            cout<<"0"<<endl;
-        }
+        cout<<resultat<<endl;
+        
         if(mostrar_cami){
 
+            for(int i=0;i<rows;i++){
 
+                for(int j=0;j<cols;j++){
+                    if(aux_matriu[i][j]==0){
+                        cout<<"*";
+                    }else{
+                        cout<<matriu[i][j];
+                    }
+                }
+                cout<<endl;
+            }
         }
 
 
